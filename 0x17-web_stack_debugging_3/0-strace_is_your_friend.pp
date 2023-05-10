bbf49116abf78579888bc655decab31e1b7e25c2
Forster Asamany <asamanyforster@yahoo.com>
2023-05-10 17:40:24 +0000

    Replace a line in a file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,10 @@
+# Puppet script to replace a line in a file on a server
+
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+# Replace the line having "phpp" with "php"
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
+}
