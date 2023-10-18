a7be37b058bd06c49dd5086e216a1ec1a6a58ed2
Forster Asamany <asamanyforster@yahoo.com>
2023-10-18 04:42:08 -0400

    Open a file without any error message

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# This script ensures a user holberton can login and open files without error messages
+
+# Increase hard file limit for a user holberton
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for a user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
